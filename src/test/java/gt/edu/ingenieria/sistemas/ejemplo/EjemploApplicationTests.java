764afb488724320431c209ee2cc528f3e20a3bc7
bryan <bryancorado777.com>
2022-12-12 20:11:21 -0600

    first commit

---
+++ src/test/java/gt/edu/ingenieria/sistemas/ejemplo/EjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.edu.ingenieria.sistemas.ejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
